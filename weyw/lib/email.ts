3cf0e027dd9d12c047d068b330942aeeb09a078f
Oligonari2810 <olivergonzalezarias@gmail.com>
2025-12-29 00:42:44 -0400

    Delete weyw directory

--- weyw/lib/email.ts
+++
@@ -1,7 +0,0 @@
-// MVP: Simulamos con console.log
-export async function sendEmail(to: string, subject: string, html: string) {
-  console.log(`📧 Email enviado a ${to} | Asunto: ${subject}`)
-  console.log(`Contenido: ${html}`)
-  return { success: true, simulated: true }
-}
-
